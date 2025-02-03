621672bd2bdd17cd5a3c2e1c18390e9d444d7f8a
fazil6126912 <164895007+fazil6126912@users.noreply.github.com>
2025-02-03 23:50:35 +0530

    Update WelcomeScreen.jsx

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -16,7 +16,7 @@ const TypewriterEffect = ({ text }) => {
       } else {
         clearInterval(timer);
       }
-    }, 200);
+    }, 150);
     
     return () => clearInterval(timer);
   }, [text]);
